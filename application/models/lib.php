29371746320bedef69e4ed6d7caed97f9a493844
cristian <cristian.yela@hemisferiod.co>
2016-10-19 23:38:22 -0500

    CY_2016_10_19
    Se agregaron las librerias necesarias para la exportacion de reportes en
    archivos xls y pdf

--- application/models/lib.php
+++ application/models/lib.php
@@ -24,4 +24,14 @@ class lib extends CI_Model
 			return false;
 		}
 	}
+	public function css_js_tables_responsive(){
+		$content = '';
+		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/jquery-1.12.0.min.js'></script>";
+		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/jquery.dataTables.min.js'></script>";
+		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/dataTables.responsive.min.js'></script>";
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/responsive/responsive.dataTables.min.css'>";
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/responsive/jquery.dataTables.min.css'>";
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/estilotabla.css'>";
+		return $content;
+	}
 }
