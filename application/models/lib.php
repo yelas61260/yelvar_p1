45b86375981ebbd684ed234a3efda2f7babf5884
cristian <cristian.yela@hemisferiod.co>
2016-10-22 14:50:53 -0500

    CY_2016_10_22
    Se definieron las funciones para la libreria lib.php

--- application/models/lib.php
+++ application/models/lib.php
@@ -24,15 +24,8 @@ class lib extends CI_Model
 			return false;
 		}
 	}
-	public function css_js_tables_responsive(){
-		$content = '';
-		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/jquery-1.12.0.min.js'></script>";
-		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/jquery.dataTables.min.js'></script>";
-		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/dataTables.responsive.min.js'></script>";
-		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/responsive/responsive.dataTables.min.css'>";
-		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/responsive/jquery.dataTables.min.css'>";
-		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/estilotabla.css'>";
-		return $content;
+	public function print_tabla(){
+		return "tabla";
 	}
 	public function print_lista_filtrada($tabla, $campo, $get_campo, $condiciones, $order_by=null){
 		$content = '<option value="">Seleccionar</option>';
@@ -44,6 +37,30 @@ class lib extends CI_Model
 		foreach ($datos as $valor) {
 			$content .= '<option value="'.$valor[$campo[0]].'">'.$valor[$campo[1]].'</option>';
 		}
+		return $content;
+	}
+	public function print_menu(){
+		return "menu";
+	}
+	public function print_header(){
+		return "header";
+	}
+	public function print_footer(){
+		return "footer";
+	}
+	public function style_login(){
+		$content = '';
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/StyleLogin.css'>";
+		return $content;
+	}
+	public function css_js_tables_responsive(){
+		$content = '';
+		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/jquery-1.12.0.min.js'></script>";
+		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/jquery.dataTables.min.js'></script>";
+		$content .="<script type='text/javascript' src='".base_url()."recursos/js/responsive/dataTables.responsive.min.js'></script>";
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/responsive/responsive.dataTables.min.css'>";
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/responsive/jquery.dataTables.min.css'>";
+		$content .="<link rel='stylesheet' type='text/css' href='".base_url()."recursos/css/estilotabla.css'>";
 		return $content;
 	}
 }
