be5c9e3d7daf03254cfda329f1277815c486227a
holmanyelvar <yelvarit@gmail.com>
2016-10-23 19:08:18 -0500

    HV_2016_10_23
    creacion de login y empalme con  cristian yela

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -4,7 +4,13 @@ class Login extends CI_Controller {
 
 	public function index(){
 		if(empty($this->session->userdata("id"))){
-			$this->load->view('viewLogin');
+			$data= array(
+				'titulo'=>'Login',
+				'StyleView'=>$this->lib->style_login(), 
+				'Header'=>$this->lib->print_header(), 
+				'Footer'=>$this->lib->print_footer(), 
+				);
+			$this->load->view('viewLogin',$data);
 		}else{
 			header("Location: ".base_url());
 		}
