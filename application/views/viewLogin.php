a015b3afeaab4af9aaf7d0391de323b65014fe7f
holmanyelvar <yelvarit@gmail.com>
2016-11-18 01:17:18 -0500

    HV_2016_11_18
    Cargue de diseño de formulario login, ajustandose a la resolucion de
    pantalla

--- application/views/viewLogin.php
+++ application/views/viewLogin.php
@@ -1,35 +1,33 @@
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
+
 
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<title><?= $titulo ?></title>
 	<?= $StyleView ?>
-	</head>
+</head>
 <body>
 	<?= $Header ?>
-	<div class="contentarea">
-<table>
-	<tr>
-		<td>
-		<form action="<?=base_url() ?>DAOUsuarioIMPL/actionLogin"  method="POST">
-		<h1>Ingresar</h1>
-		<IMG class="userim" SRC="<?=base_url() ?>recursos/pix/Login/userim.png">
-		<Input name="p1" class="usuario" type="text" max-length="20" placeholder="Usuario"/>
-		<IMG class="lock" SRC="<?=base_url() ?>recursos/pix/Login/lock.png">
-		<Input name="p2" class="password" type="password" max-length="20" placeholder="Contraseña"/>
-		<h4 class="recordarme">Recordarme</h4><h5><a href="Olvido Contraseña">Olvido Contraseña</a></h5>
-		<button type="subimit">Entrar</Button>	
-		</form>
-		</td>	
-		<td>
-		<IMG class="pueblo" SRC="<?=base_url() ?>recursos/pix/Login/pueblo.png" ALT="">
-		</td>
-	</tr>
-</table>
-</div>
-	<?= $Footer ?>
-
-
+	<div id="etiqueta">	
+		
+			<table class="tform">
+			<tr><td class="centrar">
+				<form action="<?=base_url() ?>DAOUsuarioIMPL/actionLogin"  method="POST">
+				<h1>Ingresar</h1>
+				<IMG class="userim" SRC="<?=base_url() ?>recursos/pix/Login/userim.png">
+				<Input name="p1" class="usuario" type="text" max-length="20" placeholder="Usuario"/>
+				<IMG class="lock" SRC="<?=base_url() ?>recursos/pix/Login/lock.png">
+				<Input name="p2" class="password" type="password" max-length="20" placeholder="Contraseña"/>
+				<button type="subimit">Entrar</Button><br/>
+				<a href="Olvido Contraseña">Olvido Contraseña</a>
+				</form>
+			</td><td class="ocultar">
+				<IMG class="pueblo" SRC="<?=base_url() ?>recursos/pix/Login/pueblo.png" ALT="">
+			</td></tr>
+		</table>
+		
+	
+	</div>
 </body>
-</html>
