93e7d9a6796c6a93a214cd08a06300e78f5b228a
cristian <cristian.yela@hemisferiod.co>
2016-11-27 01:25:40 -0500

    CY_2016_11_26

--- application/models/db/DAOSolicitud.php
+++ application/models/db/DAOSolicitud.php
@@ -7,6 +7,7 @@ class DAOSolicitud extends CI_Model
 	
 	private static $tabla;
 	private static $campos;
+	private static $campos_form;
 
 	public function __construct()
 	{
@@ -23,10 +24,22 @@ class DAOSolicitud extends CI_Model
 		self::$campos[6] = "fecha";
 		self::$campos[7] = "estado_id";
 		self::$campos[8] = "descripcion";
+
+		self::$campos_form[0] = "id";
+		self::$campos_form[1] = "solicitante_id";
+		self::$campos_form[2] = "nombre";
+		self::$campos_form[3] = "despacho_id";
+		self::$campos_form[4] = "tipo_id";
+		self::$campos_form[5] = "estado_id";
+		self::$campos_form[6] = "descripcion";
 	}
 
 	public function getCampos(){
 		return self::$campos;
+	}
+
+	public function getCamposForm(){
+		return self::$campos_form;
 	}
 
 	public function insert($param){
@@ -42,7 +55,7 @@ class DAOSolicitud extends CI_Model
 	}
 
 	public function getDataFormById($id){
-		return $this->db_con->findWhere(self::$tabla, ["*"], [self::$campos[0]."=".$id])[0];
+		return $this->db_con->findWhere(self::$tabla, self::$campos_form, [self::$campos[0]."=".$id])[0];
 	}
 
 	public function getTablaVista(){
@@ -57,7 +70,7 @@ class DAOSolicitud extends CI_Model
 		return $this->lib->print_tabla(
 			[self::$tabla." AS t1",$this->db_struc->getTablas()[5]." AS t2",$this->db_struc->getTablas()[8]." AS t3",$this->db_struc->getTablas()[1]." AS t4",$this->db_struc->getTablas()[11]." AS t5"],
 			["ID", "Nombre", "Solicitante", "Colaborador", "Despacho"],
-			["t1.".self::$campos[0],"t1.".self::$campos[1],"CONCAT(t2.".$cmpSolicitante[2].", t2.".$cmpSolicitante[3].") AS solicitante","CONCAT(t3.".$cmpUsuarios[4].", t3.".$cmpUsuarios[5].") AS colaborador","t4.".$cmpDespacho[1]." AS despacho"],
+			["t1.".self::$campos[0],"t1.".self::$campos[1],"CONCAT(t2.".$cmpSolicitante[2].", ' ', t2.".$cmpSolicitante[3].") AS solicitante","CONCAT(t3.".$cmpUsuarios[4].", t3.".$cmpUsuarios[5].") AS colaborador","t4.".$cmpDespacho[1]." AS despacho"],
 			[self::$campos[0],self::$campos[1],"solicitante","colaborador","despacho"],
 			["t1.".self::$campos[2]."=t2.".$cmpSolicitante[0],"t1.".self::$campos[3]."=t3.".$cmpUsuarios[0],"t1.".self::$campos[4]."=t4.".$cmpDespacho[0],"t1.".self::$campos[2]."=t5.".$cmpEstado[0]],
 			["edit", "delete"], ["edit", "delete"], ["accion/actualizarSolicitud","DAOSolicitudIMPL/delete_fun"]);
